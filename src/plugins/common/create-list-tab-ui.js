60a55d337b0240bd6466f160b8330b19ad8a2314
Jafet Morales <jafet.aaron.morales@gmail.com>
2018-10-08 18:36:03 -0500

    Update create-list-tab-ui.js

--- src/plugins/common/create-list-tab-ui.js
+++ src/plugins/common/create-list-tab-ui.js
@@ -9,7 +9,7 @@ var showDragAndDropHint = true
 
 // internals
 
-function createListTabUi (args) {
+function createListTabUi(args) {
 
   // API
 
@@ -41,34 +41,35 @@ function createListTabUi (args) {
 
   // methods
 
-  function getSearchValue (val) {
+  function getSearchValue(val) {
 
     return searchInputEl.value
 
   }
 
-  function setSearchValue (val) {
+  function setSearchValue(val) {
 
     searchInputEl.value = val
 
   }
 
-  function setInfo (el) {
+  function setInfo(el) {
 
     listInfoEl.empty()
 
     if (el) {
       listInfoEl.append(el).show()
-    } else {
+    }
+    else {
       listInfoEl.hide()
     }
 
   }
 
-  function setList (items) {
+  function setList(items) {
 
     listItemContainerEl.empty()
-    if (items) items.forEach(function (item) {
+    if (items) items.forEach(function(item) {
 
       var itemEl = el('<div>', {
         class: 'io3d-inspector-plugins___list-item',
@@ -78,7 +79,7 @@ function createListTabUi (args) {
 
       if (item.thumb) {
         var img = el('<img>').appendTo(itemEl)
-        img.addEventListener('load', function () {
+        img.addEventListener('load', function() {
 
           // center image filling container div
           var layout = getCenteredImageLayout({
@@ -99,34 +100,60 @@ function createListTabUi (args) {
         img.src = item.thumb
       }
 
-      itemEl.addEventListener('dragstart', function onItemDragStart (e) {
+      itemEl.addEventListener('dragstart', function onItemDragStart(e) {
         if (e.stopPropagation) e.stopPropagation() // stops the browser from redirecting.
         fadeInDropPlane()
         e.dataTransfer.effectAllowed = 'move'
         e.dataTransfer.setData('text/plain', JSON.stringify(item))
+
+        console.log("dragging just started")
+        var cameraEl = document.querySelector('#camera2');
+        // var playerPosition = el.getAttribute('position');
+        // var cameraEl = document.createElement('a-camera')
+        cameraEl.setAttribute('camera', 'active', true);
+        var el = document.querySelector('#mynameis');
+        var playerPosition = el.getAttribute('position');
+
+        document.querySelector('#mynameis').setAttribute('camera', 'active', false);
+        var camera2 = cameraEl.getObject3D('camera');
+        camera2.position.set(playerPosition.x + 20, playerPosition.y + 10, playerPosition.z + 20);
+        camera2.lookAt(playerPosition);
+        camera2.updateMatrixWorld();
+
+
+
         return false
       }, false)
 
-      itemEl.addEventListener('dragend', function onItemDragEnd (e) {
+      itemEl.addEventListener('dragend', function onItemDragEnd(e) {
         if (e.stopPropagation) e.stopPropagation() // stops the browser from redirecting.
         fadeOutDropPlane()
         return false
       }, false)
 
-      itemEl.addEventListener('click', function onItemDragStart (e) {
+      itemEl.addEventListener('click', function onItemDragStart(e) {
         if (e.stopPropagation) e.stopPropagation() // stops the browser from redirecting.
+
+
+        // this.currentCameraEl = AFRAME.scenes[0].camera.el;
+        // console.log("the camera element is")
+        // console.log(this.currentCameraEl)
+        // this.EDITOR_CAMERA = this.currentCameraEl.getObject3D('camera');
+        // this.EDITOR_CAMERA.position.set(20, 10, 20);
+        // this.EDITOR_CAMERA.lookAt(new THREE.Vector3());
+        // this.EDITOR_CAMERA.updateMatrixWorld();
 
         // add to screen center
         var position = pickPointOnGroundPlane({
           normalizedX: 0,
-          normalizedY: 0,
-          camera: AFRAME.INSPECTOR.EDITOR_CAMERA
+          normalizedY: 0 //,
+          // camera: this.EDITOR_CAMERA //AFRAME.INSPECTOR.EDITOR_CAMERA
         })
 
-        onItemDropCallback(item, position, function (){
+        onItemDropCallback(item, position, function() {
           // show drag & drop hint after loading success message
           if (showDragAndDropHint) {
-            setTimeout(function(){
+            setTimeout(function() {
               io3d.utils.ui.message('Hint: You can use drag & drop ;)', 6000)
               showDragAndDropHint = false
             }, 1000)
@@ -140,7 +167,7 @@ function createListTabUi (args) {
 
   }
 
-  function init () {
+  function init() {
 
     tab = createTabUi()
 
@@ -176,21 +203,21 @@ function createListTabUi (args) {
     }).appendTo(document.body)
     dropPlaneEl.addEventListener('dragover', onItemDragOver, false)
     dropPlaneEl.addEventListener('drop', onItemDrop, false)
-    
+
     if (onSearchInputCallback || onSearchChangeCallback) {
 
       // add search bar
-      
+
       searchInputEl = el('<input>', {
         id: 'io3d-inspector-plugins___list-tab___search-input',
         placeholder: 'Search...'
       }).appendTo(headerEl)
 
-      if (onSearchChangeCallback) searchInputEl.addEventListener('change', function () {
+      if (onSearchChangeCallback) searchInputEl.addEventListener('change', function() {
         onSearchChangeCallback(searchInputEl.value)
       })
 
-      if (onSearchInputCallback) searchInputEl.addEventListener('input', function () {
+      if (onSearchInputCallback) searchInputEl.addEventListener('input', function() {
         onSearchInputCallback(searchInputEl.value)
       })
 
@@ -200,7 +227,8 @@ function createListTabUi (args) {
 
       headerEl.style.height = listContainerEl.style.top = '68px'
 
-    } else {
+    }
+    else {
 
       // no search bar
 
@@ -209,7 +237,7 @@ function createListTabUi (args) {
     }
 
     // overlay plane for drag and drop
-    
+
     el('<div>', {
       id: 'io3d-inspector-plugins___list-tab___drop-plane-info',
       text: 'drop here'
@@ -219,17 +247,17 @@ function createListTabUi (args) {
 
   }
 
-  function onItemDragOver (e) {
+  function onItemDragOver(e) {
 
     if (e.preventDefault) e.preventDefault() // Necessary. Allows us to drop.
 
-    e.dataTransfer.dropEffect = 'move'  // See the section on the DataTransfer object.
+    e.dataTransfer.dropEffect = 'move' // See the section on the DataTransfer object.
 
     return false
 
   }
 
-  function onItemDrop (e) {
+  function onItemDrop(e) {
 
     if (e.preventDefault) e.preventDefault() // stops the browser from redirecting.
     if (e.stopPropagation) e.stopPropagation() // stops the browser from redirecting.
@@ -237,52 +265,135 @@ function createListTabUi (args) {
     // hide dropPlaneEl
     fadeOutDropPlane()
 
+
+    // this.currentCameraEl = AFRAME.scenes[0].camera.el;
+    // console.log("the camera element is")
+    // console.log(this.currentCameraEl)
+    // this.EDITOR_CAMERA = this.currentCameraEl.getObject3D('camera');
+    // // this.EDITOR_CAMERA.position.set(20, 10, 20);
+    // // this.EDITOR_CAMERA.lookAt(new THREE.Vector3());
+    // this.EDITOR_CAMERA.updateMatrixWorld();
+
+
+
+
+    // var camera = AFRAME.scenes[0].camera.el.getObject3D('camera')
+    var el = document.querySelector('#mynameis');
+    var playerPosition = el.getAttribute('position');
+    var cameraEl = document.createElement('a-camera')
+    cameraEl.setAttribute('camera', 'active', true);
+    document.querySelector('a-scene').appendChild(cameraEl);
+    // cameraEl.addEventListener('loaded', function() {
+    console.log('temporary camera attached');
+    // var camera2 = cameraEl.getObject3D('camera');
+    // camera2.position.set(playerPosition.x+20, playerPosition.y+10, playerPosition.z+20);
+    // camera2.lookAt(playerPosition);
+    // camera2.updateMatrixWorld();
+
+
+    console.log("Jafet says user position is")
+    console.log(playerPosition)
+
+    console.log("e is:")
+    console.log(e)
     // get picking point
+
+
+    var cameraEl = document.querySelector('#camera2');
+    var camera2 = cameraEl.getObject3D('camera');
+    console.log("Jafet says camera 2 position is")
+    console.log(camera2.position)
+
+
+
+
+
     var position = pickPointOnGroundPlane({
       x: e.x,
       y: e.y,
-      canvas: AFRAME.scenes[0].canvas,
-      camera: AFRAME.INSPECTOR.EDITOR_CAMERA
+      canvas: AFRAME.scenes[0].canvas, //,
+      tempCamera: camera2, //AFRAME.INSPECTOR.EDITOR_CAMERA
+      playerPosition:playerPosition
     })
+    // var position = pickPointOnGroundPlane({
+    //   x: playerPosition.x,
+    //   y: playerPosition.y,
+    //   canvas: AFRAME.scenes[0].canvas,//,
+    //   tempCamera: camera2 //AFRAME.INSPECTOR.EDITOR_CAMERA
+    // })
 
+
+    console.log("Jafet says drop off location is:")
+    console.log(position)
+    
+    position.y=1
     // get item data
     var item = JSON.parse(e.dataTransfer.getData('text/plain'))
+    onItemDropCallback(item, position, function() {
+      cameraEl.setAttribute('camera', 'active', false);
+      el.setAttribute('camera', 'active', true);
 
-    onItemDropCallback(item, position, function(){})
+    })
+    // onItemDropCallback(item, position, function() {})
+
+    // cameraEl.setAttribute('camera', 'active', false);
+    // el.setAttribute('camera', 'active', true);
+    // el.getObject3D('camera').updateMatrixWorld();
+
+    // });
+
+
+
+
+
+    // // get picking point
+    // var position = pickPointOnGroundPlane({
+    //   x: e.x,
+    //   y: e.y,
+    //   canvas: AFRAME.scenes[0].canvas//,
+    //   tempCamera: camera2 //AFRAME.INSPECTOR.EDITOR_CAMERA
+    // })
+
+    // console.log("Jafet says the position is:")
+    // console.log(position)
+
+    // // get item data
+    // var item = JSON.parse(e.dataTransfer.getData('text/plain'))
+    // onItemDropCallback(item, position, function() {})
 
     return false
   }
 
-  function fadeInDropPlane () {
+  function fadeInDropPlane() {
     dropPlaneEl.style.display = 'block'
-    setTimeout(function () {
+    setTimeout(function() {
       dropPlaneEl.style.opacity = 1
     }, 50)
   }
 
-  function fadeOutDropPlane () {
+  function fadeOutDropPlane() {
     dropPlaneEl.style.opacity = 0
-    setTimeout(function () {
+    setTimeout(function() {
       dropPlaneEl.style.display = 'none'
     }, 300)
   }
 
-  function show (callback, animate) {
+  function show(callback, animate) {
     if (!isInitialized) init()
-    tab.slideIn(function () {
+    tab.slideIn(function() {
       focusSearchEl()
       if (typeof callback === 'function') callback()
     }, animate)
   }
 
-  function hide (callback, animate) {
+  function hide(callback, animate) {
     if (typeof onHide === 'function') onHide()
     tab.slideOut(callback, animate)
   }
 
-  function focusSearchEl () {
+  function focusSearchEl() {
     if (searchInputEl) {
-      setTimeout(function () {
+      setTimeout(function() {
         searchInputEl.focus()
         searchInputEl.selectionStart = 10000
         searchInputEl.selectionEnd = 10000
